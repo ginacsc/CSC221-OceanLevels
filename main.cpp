e652d014252ea895cf0b4a2d3ca4ec9f4d7bbf43
ginacsc <gc16189@email.vccs.edu>
2026-09-16 20:30:21 -0400

    Moved program comment.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
+// This program calculates ocean level increases over several years.
+
 #include <iostream>
 using namespace std;
-
-// This program calculates ocean level increases over several years.
 
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
